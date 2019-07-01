a95af30ddb7fa433f10567379b02ea9ec0b89442
yasirAlkahtani <52216321+yasirAlkahtani@users.noreply.github.com>
2019-07-01 20:05:49 +0200

    Create prima.cpp

---
+++ prima.cpp
@@ -0,0 +1,26 @@
+/*
+created by Muhammad Yasir
+Namefile: while.cpp
+show prima number which smaller then number
+Date: 31.06.2019
+*/
+
+#include <iostream>
+
+int main () {
+   // Local variable declaration:
+   int prima, check = 0,i = 1;
+
+    std::cout<<"Input Number: ";
+    std::cin>>prima;
+
+   // while loop execution
+   while(i <= prima) {
+        if(prima%i==0)check++;
+    i++;
+   }
+   if (check==2)std::cout<<"PRIMA"<<std::endl;
+   else std::cout<<"Not PRIMA"<<std::endl;
+
+   return 0;
+}
